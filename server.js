acd65108ad64dfb8bac5473f4c797671d661f2aa
Solmarnov <solmarnov@gmail.com>
2020-07-30 14:13:47 +1000

    Adds Presentation

--- server.js
+++ server.js
@@ -15,10 +15,9 @@ if (process.env.NODE_ENV === 'production') {
 app.use(routes);
 
 // Connect to the Mongo DB
-mongoose.set('useUnifiedTopology', true);
 mongoose.connect(
   process.env.MONGODB_URI || 'mongodb://localhost/redseaband',
-  { useNewUrlParser: true }
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
 
 // Start the API server
